c30e73a33b08a5fcaf16ccbc493a83cf9fa6fcdb
Quinn Cheng <sylafari@gmail.com>
2020-11-12 11:17:35 -0800

    Mongodb initiallized

--- app.js
+++ app.js
@@ -1,18 +1,76 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb', { useUnifiedTopology: true, useNewUrlParser: true });
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', ()=>{
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', (err)=>{
+    console.log(err);
+});
 
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//BodyParser
+app.use(express.urlencoded({extended:false}));
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home Route
 app.get('/', (req, res) => {
-    res.render('index');
+    Article.find({}, (err, articles)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render('index', {
+        title: 'Articles',
+        articles: articles
+        });
+        }
+        
+    });
+    
 });
 
+// Add Route
+app.get('/articles/add', (req,res)=>
+{
+    res.render('add_article', {
+        title: 'Add Article'
+    })
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req,res)=>{
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err)=>{
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 // Start Server
 app.listen(3000, ()=> {
     console.log('Server started on port 3000...')
